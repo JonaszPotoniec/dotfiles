a2f860c8109b6fec5da981b8259537380590fbae
Jonasz Potoniec <jonasz.potoniec@gmail.com>
2021-02-19 15:09:45 +0100

    Create user.js

---
+++ firefox/user.js
@@ -0,0 +1,7 @@
+user_pref("gfx.webrender.all", true);
+user_pref("ui.systemUsesDarkTheme", 1);
+user_pref("browser.in-content.dark-mode", true);
+user_pref("devtools.theme", "dark");
+user_pref("devtools.toolbox.host", "right");
+user_pref("browser.link.open_newwindow.restriction", 0);
+user_pref("browser.link.open_newwindow", 3);
